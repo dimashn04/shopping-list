164c2f90b29f3d7122b61a6488151e2fec626a92
DimasHN_04 <dimashn04@gmail.com>
2023-11-22 15:00:40 +0700

    Fix @shopping_list:settings.py

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -22,6 +22,9 @@ rs.default_port='8080'
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env() # Tambahkan kode berikut
 
+CSRF_TRUSTED_ORIGINS = ['http://dimas-herjunodarpito-tutorial.pbp.cs.ui.ac.id', 
+                        'https://dimas-herjunodarpito-tutorial.pbp.cs.ui.ac.id']
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
